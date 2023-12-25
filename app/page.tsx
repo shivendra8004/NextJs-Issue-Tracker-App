8d10a37f9cd7a7a16c734b08f43906f518889bf7
SHIVENDRA8004 <jat13632@gmail.com>
2023-12-25 11:53:47 +0530

    Feat: Learned about css modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ProductCard from "./components/ProductCard";
+import ProductCard from "./components/ProductCard/ProductCard";
 import Link from "next/link";
 export default function Home() {
     return (
